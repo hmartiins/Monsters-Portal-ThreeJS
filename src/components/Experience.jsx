5165d4b0023fc8707b13dc0230a5d9de733b24b8
hmartiins <hmartins224@gmail.com>
2023-08-26 13:51:40 -0300

    feat: add textures

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three";
 
 export const Experience = () => {
+  const map = useTexture('textures/anime_art_style_a_water_based_pokemon_like_environ.jpg')
+
   return (
     <>
       <OrbitControls />
+
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
+      
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <sphereGeometry args={[5, 64, 64]} />
+        <meshStandardMaterial side={THREE.BackSide} map={map} />
       </mesh>
     </>
   );
