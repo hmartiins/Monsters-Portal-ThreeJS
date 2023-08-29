3c9483e6b5d60e2f2865821c620d4430c4d964fa
hmartiins <hmartins224@gmail.com>
2023-08-29 19:26:53 -0300

    feat: create rounded box monster portal

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,7 @@
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture } from "@react-three/drei";
 import * as THREE from "three";
+
+import { Fish } from './Fish'
 
 export const Experience = () => {
   const map = useTexture('textures/anime_art_style_a_water_based_pokemon_like_environ.jpg')
@@ -11,10 +13,20 @@ export const Experience = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       
-      <mesh>
-        <sphereGeometry args={[5, 64, 64]} />
-        <meshStandardMaterial side={THREE.BackSide} map={map} />
-      </mesh>
+      <RoundedBox args={[2, 3, 0.1]}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
+          <ambientLight intensity={0.5} />
+          <Environment preset="sunset" />
+
+          <Fish scale={0.6} position-y={-1} />
+          
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial side={THREE.BackSide} map={map} />
+          </mesh>
+        </MeshPortalMaterial>     
+      </RoundedBox>
+
     </>
   );
 };
