ac2dd19979b6877042ac9b2f96291ffc972e1f09
hmartiins <hmartins224@gmail.com>
2023-09-02 00:15:29 -0300

    feat: add stars background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Stars } from "./components/Stars";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
       <Experience />
+      <Stars />
     </Canvas>
   );
 }
